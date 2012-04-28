04a1b0b8c74d0a99d3f9d76a900003bcc23087a3
Valeriy Reutov <valeriy.reutov@gmail.com>
2012-04-28 18:13:33 +0300

    xml schema patterns corrected

--- tools/xml_library/sources/resources/xl_resources.cpp
+++ tools/xml_library/sources/resources/xl_resources.cpp
@@ -18,9 +18,9 @@ namespace Strings {
 
 	const char* const XmlSchemaDocumentFormat = "<?xml version=\"1.0\" encoding=\"utf-8\"?><xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">%1</xsd:schema>";
 
-	const char* const ComplexTypeFormat = "<xsd::complexType>%2</xsd::complexType>";
+	const char* const ComplexTypeFormat = "<xsd:complexType>%2</xsd:complexType>";
 
-	const char* const ElementFormat = "<xsd::element name=\"%1\"/>";
+	const char* const ElementFormat = "<xsd:element name=\"%1\"/>";
 
 /*---------------------------------------------------------------------------*/
 
