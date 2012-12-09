4dea2de97f45b46638f3c7b1e85b189bf0fe2e08
Konstantin <slipkid85@gmail.com>
2012-12-09 23:02:03 +0200

    create console messenger

---
+++ plugins/gui/console/sources/console_messenger/con_console_messenger.cpp
@@ -0,0 +1,37 @@
+
+#include "console/sources/ph/con_ph.hpp"
+
+#include "con_console_messenger.hpp"
+
+/*---------------------------------------------------------------------------*/
+
+namespace Plugins {
+namespace GUI {
+namespace Console {
+
+/*---------------------------------------------------------------------------*/
+
+ConsoleMessenger::ConsoleMessenger()
+{
+} // ConsoleMessenger::ConsoleMessenger
+
+/*---------------------------------------------------------------------------*/
+
+ConsoleMessenger::~ConsoleMessenger()
+{
+} // ConsoleMessenger::~ConsoleMessenger
+
+/*---------------------------------------------------------------------------*/
+
+void ConsoleMessenger::printMessege(
+		const Tools::Core::IMessenger::MessegeLevel::Enum _messegeLevel
+	,	const std::string& _messege )
+{
+} // ConsoleMessenger::printMessege
+
+/*---------------------------------------------------------------------------*/
+
+} // Console
+} // GUI
+} // Plugins
+
