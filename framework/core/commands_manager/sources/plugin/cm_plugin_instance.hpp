477e8851113b6b0cf5f86f4f1ca104da907b8809
Valeriy Reutov <valeriy.reutov@gmail.com>
2012-12-09 22:30:09 +0200

    - added commands manager plugin - corrected 'Release' configuration

---
+++ framework/core/commands_manager/sources/plugin/cm_plugin_instance.hpp
@@ -0,0 +1,54 @@
+
+#ifndef __CM_PLUGIN_INSTANCE_HPP__
+#define __CM_PLUGIN_INSTANCE_HPP__
+
+/*---------------------------------------------------------------------------*/
+
+#include "plugins_manager/h/pm_base_plugin.hpp"
+#include "plugins_manager/h/pm_interface_map.hpp"
+
+/*---------------------------------------------------------------------------*/
+
+namespace Framework {
+namespace Core {
+namespace CommandsManager {
+
+/*---------------------------------------------------------------------------*/
+
+class PluginInstance
+	:	public Framework::Core::PluginsManager::BasePlugin
+{
+
+/*---------------------------------------------------------------------------*/
+
+public:
+
+/*---------------------------------------------------------------------------*/
+
+	PluginInstance();
+
+	virtual ~PluginInstance();
+
+/*---------------------------------------------------------------------------*/
+
+	INTERFACE_MAP_DECLARATION()
+
+/*---------------------------------------------------------------------------*/
+
+	/*virtual*/ void initialize();
+
+	/*virtual*/ void close();
+
+/*---------------------------------------------------------------------------*/
+
+};
+
+/*---------------------------------------------------------------------------*/
+
+} // namespace CommandsManager
+} // namespace Core
+} // namespace Framework
+
+/*---------------------------------------------------------------------------*/
+
+#endif // __CM_PLUGIN_INSTANCE_HPP__
