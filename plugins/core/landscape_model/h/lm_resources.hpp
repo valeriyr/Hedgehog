c9a5b5315949c0fec1f913d8402d93c4aa70c9c4
valeriyr <valeriy.reutov@gmail.com>
2013-12-08 00:40:16 +0200

    added external resources to the landscape_model plugin

---
+++ plugins/core/landscape_model/h/lm_resources.hpp
@@ -0,0 +1,25 @@
+
+#ifndef __LM_RESOURCES_HPP__
+#define __LM_RESOURCES_HPP__
+
+/*---------------------------------------------------------------------------*/
+
+namespace Plugins {
+namespace Core {
+namespace LandscapeModel {
+namespace Resources {
+
+/*---------------------------------------------------------------------------*/
+
+	const char* const ModelThreadName = "ModelThread";
+
+/*---------------------------------------------------------------------------*/
+
+} // namespace Resources
+} // namespace LandscapeModel
+} // namespace Core
+} // namespace Plugins
+
+/*---------------------------------------------------------------------------*/
+
+#endif // __LM_RESOURCES_HPP__
