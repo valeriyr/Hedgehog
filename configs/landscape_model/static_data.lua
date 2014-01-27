588fcf18a54c0407e2f4e0d2d2833aaf04db2ad8
valeriyr <valeriy.reutov@gmail.com>
2014-01-27 20:43:06 +0200

    added Peon

--- configs/landscape_model/static_data.lua
+++ configs/landscape_model/static_data.lua
@@ -4,6 +4,30 @@
 StaticData:regResource( QString( "Gold" ) )
 StaticData:regResource( QString( "Wood" ) )
 
+-- Peon
+
+peon = ObjectStaticData()
+
+peon.m_healthData = HealthComponentStaticData( 50 )
+peon.m_locateData = LocateComponentStaticData( QSize( 1, 1 ), TerrainMapItem.Ground, Emplacement.Ground )
+peon.m_moveData = MoveComponentStaticData( 500 )
+peon.m_selectionData = SelectionComponentStaticData( true )
+peon.m_attackData = AttackComponentStaticData(  4, 6, 1.0, 400, 1500 )
+
+peon.m_actionsData = ActionsComponentStaticData()
+peon.m_actionsData:can( Actions.Move )
+peon.m_actionsData:can( Actions.Attack )
+peon.m_actionsData:can( Actions.Build )
+
+orcBarracksResourceData = ResourcesData()
+orcBarracksResourceData:pushResource( QString( "Gold" ), 400 )
+orcBarracksResourceData:pushResource( QString( "Wood" ), 200 )
+
+peon.m_buildData = BuildComponentStaticData()
+peon.m_buildData:pushBuildData( QString( "Orc Barracks" ), BuildData( 60000, orcBarracksResourceData ) )
+
+StaticData:regObjectStaticData( QString( "Peon" ), peon )
+
 -- Elven Archer
 
 elvenArcher = ObjectStaticData()
